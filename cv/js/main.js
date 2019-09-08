7c0f30851380330d47104336571412e2481e6eb9
MaksymKutelmakh <50178880+MaksymKutelmakh@users.noreply.github.com>
2019-09-08 23:24:47 +0300

    new change in cv

--- cv/js/main.js
+++ cv/js/main.js
@@ -3,3 +3,30 @@ var wrapperMenu = document.querySelector('.wrapper-menu');
 wrapperMenu.addEventListener('click', function(){
   wrapperMenu.classList.toggle('open');  
 })
+
+
+function hideLoader(){
+  setTimeout(
+    function(){
+      $('.loader').hide("drop", { direction: "down" }, "slow");
+    }
+  ,5000)
+  
+}
+
+setTimeout(
+  function(){
+    $( ".nav" ).show("drop",  { direction: "up" }, "slow", flex);
+    setInterval(
+      flex()
+    ,1);
+    $(".greet").show("drop", { direction: "down" }, "slow");
+    setTimeout(
+      $('.i').show()
+    ,1)
+  }
+,6500);
+
+function flex(){
+  $('.nav').css('display','flex');
+}
