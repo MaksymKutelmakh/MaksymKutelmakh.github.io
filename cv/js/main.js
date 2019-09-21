008168cbae0dbf6dc6f8f4b80724227d11ac3cad
MaksymKutelmakh <50178880+MaksymKutelmakh@users.noreply.github.com>
2019-09-21 18:21:10 +0300

    'TEST'

--- cv/js/main.js
+++ cv/js/main.js
@@ -11,12 +11,12 @@ function hideLoader(){
       $('.loader').hide("drop", { direction: "down" }, "slow");
     }
   ,2500)
-  
 }
 
 setTimeout(
   function(){
     $( ".nav" ).show("drop",  { direction: "up" }, "slow", flex);
+    togglePlay();
     setInterval(
       flex()
     ,1);
@@ -33,9 +33,7 @@ function flex(){
   $('.i').css('display','flex');
 }
 
-function flexForWrapper(){
-  $('.wrapper').css('display','flex');
-}
+
 
 function getCoords(elem) { // кроме IE8-
   var box = elem.getBoundingClientRect();
@@ -54,11 +52,93 @@ window.addEventListener('scroll', function(elem) {
       $('.hero__content h2').show("drop", { direction: "left" }, "slow");
     }
 
-    if(this.pageYOffset > '600'){
-      $('.wrapper').show("drop", { direction: "right" }, "slow");
-      setInterval(
-        flexForWrapper()
-      ,1);
+    if(this.pageYOffset > '700' && this.pageYOffset < '744'){
+      $('.wrapper').addClass('span');
+      
     }
    
+});
+
+var music = document.getElementById("music");
+var isPlaying = false;
+music.volume = 0.2;
+function togglePlay() {
+  if (isPlaying) {
+    music.pause()
+  } else {
+    music.play();
+  }
+};
+music.onplaying = function() {
+  isPlaying = true;
+  document.getElementById("music-animation").classList.add('on')
+};
+music.onpause = function() {
+  isPlaying = false;
+  document.getElementById("music-animation").classList.remove('on')
+};
+
+var button = document.getElementById("toggle");
+button.addEventListener('click', function() {
+  if (button.getAttribute("data-text-swap") == button.innerHTML) {
+    button.innerHTML = button.getAttribute("data-text-original");
+  } else {
+    button.setAttribute("data-text-original", button.innerHTML);
+    button.innerHTML = button.getAttribute("data-text-swap");
+  }
+}, false);
+
+var swiper = new Swiper('.product-slider', {
+  spaceBetween: 30,
+  effect: 'fade',
+  // initialSlide: 2,
+  loop: false,
+  navigation: {
+      nextEl: '.next',
+      prevEl: '.prev'
+  },
+  // mousewheel: {
+  //     // invert: false
+  // },
+  on: {
+      init: function(){
+          var index = this.activeIndex;
+
+          var target = $('.product-slider__item').eq(index).data('target');
+
+          console.log(target);
+
+          $('.product-img__item').removeClass('active');
+          $('.product-img__item#'+ target).addClass('active');
+      }
+  }
+
+});
+
+swiper.on('slideChange', function () {
+  var index = this.activeIndex;
+
+  var target = $('.product-slider__item').eq(index).data('target');
+
+  console.log(target);
+
+  $('.product-img__item').removeClass('active');
+  $('.product-img__item#'+ target).addClass('active');
+
+  if(swiper.isEnd) {
+      $('.prev').removeClass('disabled');
+      $('.next').addClass('disabled');
+  } else {
+      $('.next').removeClass('disabled');
+  }
+
+  if(swiper.isBeginning) {
+      $('.prev').addClass('disabled');
+  } else {
+      $('.prev').removeClass('disabled');
+  }
+});
+
+$(".js-fav").on("click", function() {
+  $(this).find('.heart').toggleClass("is-active");
 });
