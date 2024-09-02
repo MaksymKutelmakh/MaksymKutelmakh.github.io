bae63596bf8fe8d8981b3d083844632064060097
MaksymKutelmakh <maksymkutelmah@gmail.com>
2024-09-02 17:28:27 +0300

    make contacts active

--- oratory.savio/assets/js/main.js
+++ oratory.savio/assets/js/main.js
@@ -183,6 +183,7 @@
    */
   function initSwiper() {
     document.querySelectorAll(".init-swiper").forEach(function(swiperElement) {
+      
       let config =
           {
             loop: true,
